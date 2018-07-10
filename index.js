ca27eca3d6d56fc49c79f4c073f5a21319c74907
Ritam Majjumdar <majumdar_ritam@yahoo.com>
2018-07-10 14:08:34 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -5,3 +5,21 @@
  * in english language to that in the equivalent braille
  * code.
  */
+import englishToBrailleLiteralSet from './english-to-braille.js';
+
+const set = new Map(englishToBrailleLiteralSet);
+
+function converter() {
+  const s = document.getElementById('sourceLangText').value;
+  let result = '';
+  let i = 0;
+  for (i = 0; i < s.length; i += 1) {
+    result += set.get(s[i]);
+  }
+  document.getElementById('targetLangText').innerHTML = result;
+}
+
+window.onload = function () {
+  const btn = document.getElementById('btnConvertEnglishToBraille');
+  btn.addEventListener('click', converter);
+};
